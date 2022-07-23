ae63efc430abd07010383be4fd7df26b94ff4d06
David R <david.abril22@gmail.com>
2022-07-23 17:37:17 +0200

    Added EnumDisplaySettings pinvoke user32.dll method with its struct.

--- User32.cs
+++ User32.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Windows.Forms;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +12,46 @@ namespace KimeraCS
     {
         internal const string DLLName = "USER32.DLL";
 
+        [StructLayout(LayoutKind.Sequential)]
+        public struct DEVMODE
+        {
+            private const int CCHDEVICENAME = 0x20;
+            private const int CCHFORMNAME = 0x20;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 0x20)]
+            public string dmDeviceName;
+            public short dmSpecVersion;
+            public short dmDriverVersion;
+            public short dmSize;
+            public short dmDriverExtra;
+            public int dmFields;
+            public int dmPositionX;
+            public int dmPositionY;
+            public ScreenOrientation dmDisplayOrientation;
+            public int dmDisplayFixedOutput;
+            public short dmColor;
+            public short dmDuplex;
+            public short dmYResolution;
+            public short dmTTOption;
+            public short dmCollate;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 0x20)]
+            public string dmFormName;
+            public short dmLogPixels;
+            public int dmBitsPerPel;
+            public int dmPelsWidth;
+            public int dmPelsHeight;
+            public int dmDisplayFlags;
+            public int dmDisplayFrequency;
+            public int dmICMMethod;
+            public int dmICMIntent;
+            public int dmMediaType;
+            public int dmDitherType;
+            public int dmReserved1;
+            public int dmReserved2;
+            public int dmPanningWidth;
+            public int dmPanningHeight;
+        }
+
+
         [DllImport(DLLName, EntryPoint = "GetDC", SetLastError = true)]
         public static extern IntPtr GetDC(IntPtr hwnd);
 
@@ -20,6 +61,10 @@ namespace KimeraCS
         [return: MarshalAs(UnmanagedType.Bool)]
         [DllImport(DLLName, EntryPoint = "BlockInput", CharSet = CharSet.Auto, ExactSpelling = true)]
         public static extern void BlockInput([In, MarshalAs(UnmanagedType.Bool)] bool fBlockIt);
+
+        [DllImport(DLLName)]
+        public static extern bool EnumDisplaySettings(string lpszDeviceName, int iModeNum, ref DEVMODE lpDevMode);
+
 
         // FolderBrowserDialogEx
         [DllImport(DLLName, EntryPoint = "SendMessageW")]
