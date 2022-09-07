674ae4a5a531e804c7127a35416eb1a4ee7f4227
David R <david.abril22@gmail.com>
2022-09-07 23:11:44 +0200

    NEW FEATURE: Magic Database.

---
+++ frmMagicDB.Designer.cs
@@ -0,0 +1,156 @@
+﻿
+namespace KimeraCS
+{
+    partial class frmMagicDB
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMagicDB));
+            this.lbMagic = new System.Windows.Forms.ListBox();
+            this.btnLoadModelAnimation = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnSaveMagicDataDir = new System.Windows.Forms.Button();
+            this.btnSelectDirBrowser = new System.Windows.Forms.Button();
+            this.txtMagicDataDir = new System.Windows.Forms.TextBox();
+            this.lblMagicDataDir = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbMagic
+            // 
+            this.lbMagic.FormattingEnabled = true;
+            this.lbMagic.ItemHeight = 16;
+            this.lbMagic.Location = new System.Drawing.Point(12, 11);
+            this.lbMagic.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.lbMagic.Name = "lbMagic";
+            this.lbMagic.Size = new System.Drawing.Size(574, 404);
+            this.lbMagic.TabIndex = 7;
+            this.lbMagic.DoubleClick += new System.EventHandler(this.lbMagic_DoubleClick);
+            // 
+            // btnLoadModelAnimation
+            // 
+            this.btnLoadModelAnimation.Location = new System.Drawing.Point(217, 491);
+            this.btnLoadModelAnimation.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnLoadModelAnimation.Name = "btnLoadModelAnimation";
+            this.btnLoadModelAnimation.Size = new System.Drawing.Size(180, 42);
+            this.btnLoadModelAnimation.TabIndex = 24;
+            this.btnLoadModelAnimation.Text = "Load Model";
+            this.btnLoadModelAnimation.UseVisualStyleBackColor = true;
+            this.btnLoadModelAnimation.Click += new System.EventHandler(this.btnLoadModelAnimation_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnClose.Location = new System.Drawing.Point(400, 491);
+            this.btnClose.Margin = new System.Windows.Forms.Padding(4);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(180, 42);
+            this.btnClose.TabIndex = 23;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnSaveMagicDataDir
+            // 
+            this.btnSaveMagicDataDir.Location = new System.Drawing.Point(12, 491);
+            this.btnSaveMagicDataDir.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnSaveMagicDataDir.Name = "btnSaveMagicDataDir";
+            this.btnSaveMagicDataDir.Size = new System.Drawing.Size(201, 42);
+            this.btnSaveMagicDataDir.TabIndex = 22;
+            this.btnSaveMagicDataDir.Text = "Save Magic Data Directory";
+            this.btnSaveMagicDataDir.UseVisualStyleBackColor = true;
+            this.btnSaveMagicDataDir.Click += new System.EventHandler(this.btnSaveMagicDataDir_Click);
+            // 
+            // btnSelectDirBrowser
+            // 
+            this.btnSelectDirBrowser.Location = new System.Drawing.Point(540, 454);
+            this.btnSelectDirBrowser.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnSelectDirBrowser.Name = "btnSelectDirBrowser";
+            this.btnSelectDirBrowser.Size = new System.Drawing.Size(40, 25);
+            this.btnSelectDirBrowser.TabIndex = 21;
+            this.btnSelectDirBrowser.Text = "...";
+            this.btnSelectDirBrowser.UseVisualStyleBackColor = true;
+            this.btnSelectDirBrowser.Click += new System.EventHandler(this.btnSelectDirBrowser_Click);
+            // 
+            // txtMagicDataDir
+            // 
+            this.txtMagicDataDir.Location = new System.Drawing.Point(12, 454);
+            this.txtMagicDataDir.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtMagicDataDir.Name = "txtMagicDataDir";
+            this.txtMagicDataDir.Size = new System.Drawing.Size(521, 22);
+            this.txtMagicDataDir.TabIndex = 20;
+            // 
+            // lblMagicDataDir
+            // 
+            this.lblMagicDataDir.AutoSize = true;
+            this.lblMagicDataDir.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblMagicDataDir.Location = new System.Drawing.Point(11, 435);
+            this.lblMagicDataDir.Name = "lblMagicDataDir";
+            this.lblMagicDataDir.Size = new System.Drawing.Size(144, 17);
+            this.lblMagicDataDir.TabIndex = 19;
+            this.lblMagicDataDir.Text = "Magic Data Directory:";
+            // 
+            // frmMagicDB
+            // 
+            this.AcceptButton = this.btnLoadModelAnimation;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.CancelButton = this.btnClose;
+            this.ClientSize = new System.Drawing.Size(599, 544);
+            this.Controls.Add(this.btnLoadModelAnimation);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnSaveMagicDataDir);
+            this.Controls.Add(this.btnSelectDirBrowser);
+            this.Controls.Add(this.txtMagicDataDir);
+            this.Controls.Add(this.lblMagicDataDir);
+            this.Controls.Add(this.lbMagic);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmMagicDB";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "FF7 Magic Database";
+            this.Load += new System.EventHandler(this.frmMagicDB_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbMagic;
+        private System.Windows.Forms.Button btnLoadModelAnimation;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnSaveMagicDataDir;
+        private System.Windows.Forms.Button btnSelectDirBrowser;
+        private System.Windows.Forms.TextBox txtMagicDataDir;
+        private System.Windows.Forms.Label lblMagicDataDir;
+    }
+}
---
+++ frmMagicDB.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KimeraCS
+{
+    using static FileTools;
+
+    public partial class frmMagicDB : Form
+    {
+        public static string strMagicFile = "", strMagicAnimFile = "";
+        public static string strLocalMagicModelName = "";
+
+        public static bool bSelectedMagicFileFromDB;
+
+        public frmMagicDB()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMagicDB_Load(object sender, EventArgs e)
+        {
+            int mi;
+
+            bSelectedMagicFileFromDB = false;
+
+            if (strMagicLGPPathSrc == "") strMagicLGPPathSrc = strGlobalPath;
+            txtMagicDataDir.Text = strMagicLGPPathSrc;
+
+            lbMagic.Items.Clear();
+
+            if (bDBMagicLoaded)
+            {
+                for (mi = 0; mi < lstMagicLGPRegisters.Count; mi++)
+                    lbMagic.Items.Add(lstMagicLGPRegisters[mi].modelName);
+
+                if (strLocalMagicModelName == "")
+                {
+                    lbMagic.SelectedIndex = 0;
+                }
+                else
+                {
+                    lbMagic.SelectedIndex = lbMagic.FindStringExact(strLocalMagicModelName);
+                }
+
+                lbMagic.Select();
+            }
+        }
+
+        private void btnSelectDirBrowser_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialogEX fbdMagicDataDirectory = new FolderBrowserDialogEX();
+
+            // We must select the directory from where to read the files.
+            fbdMagicDataDirectory.folderBrowser.Description =
+                            "Select the Folder where MAGIC.LGP file has been extracted:";
+
+            fbdMagicDataDirectory.folderBrowser.RootFolder = Environment.SpecialFolder.MyComputer;
+            fbdMagicDataDirectory.folderBrowser.ShowNewFolderButton = false;
+
+            if (strMagicLGPPathSrc != "")
+            {
+                fbdMagicDataDirectory.folderBrowser.SelectedPath = strMagicLGPPathSrc;
+            }
+            else
+            {
+                fbdMagicDataDirectory.folderBrowser.SelectedPath = strGlobalPath;
+            }
+
+            fbdMagicDataDirectory.Tmr.Start();
+            if (fbdMagicDataDirectory.folderBrowser.ShowDialog() == DialogResult.OK)
+            {
+                if (fbdMagicDataDirectory.folderBrowser.SelectedPath != "")
+                {
+                    // Put Global folder for input unswizzled.
+                    strMagicLGPPathSrc = fbdMagicDataDirectory.folderBrowser.SelectedPath;
+                    txtMagicDataDir.Text = fbdMagicDataDirectory.folderBrowser.SelectedPath;
+                }
+            }
+
+            fbdMagicDataDirectory.Dispose();
+        }
+
+        private void btnSaveMagicDataDir_Click(object sender, EventArgs e)
+        {
+            WriteCFGFile();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            strLocalMagicModelName = lbMagic.Text;
+
+            if (!bSelectedMagicFileFromDB)
+            {
+                strLocalMagicModelName = "";
+            }
+        }
+
+        private void lbMagic_DoubleClick(object sender, EventArgs e)
+        {
+            if (lbMagic.SelectedIndex > -1)
+            {
+                btnLoadModelAnimation.PerformClick();
+            }
+        }
+
+        private void btnLoadModelAnimation_Click(object sender, EventArgs e)
+        {
+
+            bSelectedMagicFileFromDB = false;
+
+            if (lbMagic.Text != "")
+            {
+                strMagicFile = txtMagicDataDir.Text + "\\" + lstMagicLGPRegisters[lbMagic.SelectedIndex].fileName + ".D";
+            }
+            else
+            {
+                MessageBox.Show("You have not selected any Magic Model.", "Information", MessageBoxButtons.OK);
+                return;
+            }
+
+            strMagicAnimFile = txtMagicDataDir.Text + "\\" + lstMagicLGPRegisters[lbMagic.SelectedIndex].fileName + ".A00";
+
+            // Check existance of the files.
+            if (!File.Exists(strMagicFile))
+            {
+                MessageBox.Show("The file selected as Magic Model " + lstMagicLGPRegisters[lbMagic.SelectedIndex].fileName + " does not exists.",
+                                "Error");
+                return;
+            }
+
+            if (!File.Exists(strMagicAnimFile))
+            {
+                 MessageBox.Show("The file selected as Field Animation " + lstMagicLGPRegisters[lbMagic.SelectedIndex].fileName + ".A00" + " does not exists.",
+                                 "Error");
+                 return;
+            }
+
+            bSelectedMagicFileFromDB = true;
+
+            Close();
+        }
+
+    }
+}
