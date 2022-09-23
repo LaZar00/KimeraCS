43220c74184e3f01dc2d68e6a79c740747e4c6c2
David R <david.abril22@gmail.com>
2022-09-23 22:44:04 +0200

    New feature for do Batch Conversion of TEX files

---
+++ frmTEXToPNGBatchConversion.Designer.cs
@@ -0,0 +1,262 @@
+﻿
+namespace KimeraCS
+{
+    partial class frmTEXToPNGBatchConversion
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmTEXToPNGBatchConversion));
+            this.rtbLog = new System.Windows.Forms.RichTextBox();
+            this.gbProgress = new System.Windows.Forms.GroupBox();
+            this.lblProcessingT2P = new System.Windows.Forms.Label();
+            this.progBarTEXBatch = new System.Windows.Forms.ProgressBar();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.gbSettings = new System.Windows.Forms.GroupBox();
+            this.btnSaveLog = new System.Windows.Forms.Button();
+            this.chkShowOnlyNoProcessed = new System.Windows.Forms.CheckBox();
+            this.lblPath = new System.Windows.Forms.Label();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnSaveOptions = new System.Windows.Forms.Button();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.txtTEX2PNGBatchPath = new System.Windows.Forms.TextBox();
+            this.btnPath = new System.Windows.Forms.Button();
+            this.saveFile = new System.Windows.Forms.SaveFileDialog();
+            this.gbProgress.SuspendLayout();
+            this.gbSettings.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // rtbLog
+            // 
+            this.rtbLog.Location = new System.Drawing.Point(8, 198);
+            this.rtbLog.Name = "rtbLog";
+            this.rtbLog.ReadOnly = true;
+            this.rtbLog.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.rtbLog.Size = new System.Drawing.Size(560, 77);
+            this.rtbLog.TabIndex = 34;
+            this.rtbLog.Text = "";
+            // 
+            // gbProgress
+            // 
+            this.gbProgress.Controls.Add(this.lblProcessingT2P);
+            this.gbProgress.Controls.Add(this.progBarTEXBatch);
+            this.gbProgress.Controls.Add(this.btnCancel);
+            this.gbProgress.Enabled = false;
+            this.gbProgress.ForeColor = System.Drawing.SystemColors.Control;
+            this.gbProgress.Location = new System.Drawing.Point(8, 125);
+            this.gbProgress.Name = "gbProgress";
+            this.gbProgress.Size = new System.Drawing.Size(560, 67);
+            this.gbProgress.TabIndex = 33;
+            this.gbProgress.TabStop = false;
+            this.gbProgress.Text = "Progress";
+            // 
+            // lblProcessingT2P
+            // 
+            this.lblProcessingT2P.Location = new System.Drawing.Point(342, 15);
+            this.lblProcessingT2P.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblProcessingT2P.Name = "lblProcessingT2P";
+            this.lblProcessingT2P.Size = new System.Drawing.Size(142, 19);
+            this.lblProcessingT2P.TabIndex = 28;
+            this.lblProcessingT2P.Text = "Processing... 0000  / 0000";
+            this.lblProcessingT2P.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // progBarTEXBatch
+            // 
+            this.progBarTEXBatch.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.progBarTEXBatch.Location = new System.Drawing.Point(11, 34);
+            this.progBarTEXBatch.MarqueeAnimationSpeed = 10;
+            this.progBarTEXBatch.Name = "progBarTEXBatch";
+            this.progBarTEXBatch.Size = new System.Drawing.Size(473, 24);
+            this.progBarTEXBatch.Step = 1;
+            this.progBarTEXBatch.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progBarTEXBatch.TabIndex = 27;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Enabled = false;
+            this.btnCancel.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnCancel.Location = new System.Drawing.Point(489, 34);
+            this.btnCancel.Margin = new System.Windows.Forms.Padding(2);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(60, 24);
+            this.btnCancel.TabIndex = 26;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // gbSettings
+            // 
+            this.gbSettings.Controls.Add(this.btnSaveLog);
+            this.gbSettings.Controls.Add(this.chkShowOnlyNoProcessed);
+            this.gbSettings.Controls.Add(this.lblPath);
+            this.gbSettings.Controls.Add(this.btnClose);
+            this.gbSettings.Controls.Add(this.btnSaveOptions);
+            this.gbSettings.Controls.Add(this.btnGo);
+            this.gbSettings.Controls.Add(this.txtTEX2PNGBatchPath);
+            this.gbSettings.Controls.Add(this.btnPath);
+            this.gbSettings.ForeColor = System.Drawing.SystemColors.Control;
+            this.gbSettings.Location = new System.Drawing.Point(8, 1);
+            this.gbSettings.Name = "gbSettings";
+            this.gbSettings.Size = new System.Drawing.Size(560, 118);
+            this.gbSettings.TabIndex = 32;
+            this.gbSettings.TabStop = false;
+            this.gbSettings.Text = "Settings";
+            // 
+            // btnSaveLog
+            // 
+            this.btnSaveLog.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnSaveLog.Location = new System.Drawing.Point(342, 82);
+            this.btnSaveLog.Margin = new System.Windows.Forms.Padding(2);
+            this.btnSaveLog.Name = "btnSaveLog";
+            this.btnSaveLog.Size = new System.Drawing.Size(79, 28);
+            this.btnSaveLog.TabIndex = 26;
+            this.btnSaveLog.Text = "Save log";
+            this.btnSaveLog.UseVisualStyleBackColor = true;
+            this.btnSaveLog.Click += new System.EventHandler(this.btnSaveLog_Click);
+            // 
+            // chkShowOnlyNoProcessed
+            // 
+            this.chkShowOnlyNoProcessed.Location = new System.Drawing.Point(403, 53);
+            this.chkShowOnlyNoProcessed.Name = "chkShowOnlyNoProcessed";
+            this.chkShowOnlyNoProcessed.Size = new System.Drawing.Size(146, 24);
+            this.chkShowOnlyNoProcessed.TabIndex = 25;
+            this.chkShowOnlyNoProcessed.Text = "Log only not processed";
+            this.chkShowOnlyNoProcessed.UseVisualStyleBackColor = true;
+            // 
+            // lblPath
+            // 
+            this.lblPath.AutoSize = true;
+            this.lblPath.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblPath.Location = new System.Drawing.Point(8, 29);
+            this.lblPath.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblPath.Name = "lblPath";
+            this.lblPath.Size = new System.Drawing.Size(32, 13);
+            this.lblPath.TabIndex = 3;
+            this.lblPath.Text = "Path:";
+            // 
+            // btnClose
+            // 
+            this.btnClose.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnClose.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnClose.Location = new System.Drawing.Point(425, 82);
+            this.btnClose.Margin = new System.Windows.Forms.Padding(2);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(124, 28);
+            this.btnClose.TabIndex = 20;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnSaveOptions
+            // 
+            this.btnSaveOptions.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnSaveOptions.Location = new System.Drawing.Point(239, 82);
+            this.btnSaveOptions.Margin = new System.Windows.Forms.Padding(2);
+            this.btnSaveOptions.Name = "btnSaveOptions";
+            this.btnSaveOptions.Size = new System.Drawing.Size(99, 28);
+            this.btnSaveOptions.TabIndex = 1;
+            this.btnSaveOptions.Text = "Save options";
+            this.btnSaveOptions.UseVisualStyleBackColor = true;
+            this.btnSaveOptions.Click += new System.EventHandler(this.btnSaveOptions_Click);
+            // 
+            // btnGo
+            // 
+            this.btnGo.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnGo.Location = new System.Drawing.Point(11, 82);
+            this.btnGo.Margin = new System.Windows.Forms.Padding(2);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(224, 28);
+            this.btnGo.TabIndex = 19;
+            this.btnGo.Text = "Go!";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // txtTEX2PNGBatchPath
+            // 
+            this.txtTEX2PNGBatchPath.Location = new System.Drawing.Point(44, 26);
+            this.txtTEX2PNGBatchPath.Margin = new System.Windows.Forms.Padding(2);
+            this.txtTEX2PNGBatchPath.Name = "txtTEX2PNGBatchPath";
+            this.txtTEX2PNGBatchPath.Size = new System.Drawing.Size(471, 20);
+            this.txtTEX2PNGBatchPath.TabIndex = 2;
+            // 
+            // btnPath
+            // 
+            this.btnPath.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnPath.Location = new System.Drawing.Point(519, 26);
+            this.btnPath.Margin = new System.Windows.Forms.Padding(2);
+            this.btnPath.Name = "btnPath";
+            this.btnPath.Size = new System.Drawing.Size(30, 20);
+            this.btnPath.TabIndex = 3;
+            this.btnPath.Text = "...";
+            this.btnPath.UseVisualStyleBackColor = true;
+            this.btnPath.Click += new System.EventHandler(this.btnPath_Click);
+            // 
+            // frmTEXToPNGBatchConversion
+            // 
+            this.AcceptButton = this.btnGo;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.CancelButton = this.btnClose;
+            this.ClientSize = new System.Drawing.Size(576, 285);
+            this.Controls.Add(this.rtbLog);
+            this.Controls.Add(this.gbProgress);
+            this.Controls.Add(this.gbSettings);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "frmTEXToPNGBatchConversion";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "TEX2PNG Batch Conversion";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmTEXToPNGBatchConversion_FormClosing);
+            this.Load += new System.EventHandler(this.frmTEXToPNGBatchConversion_Load);
+            this.gbProgress.ResumeLayout(false);
+            this.gbSettings.ResumeLayout(false);
+            this.gbSettings.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtbLog;
+        private System.Windows.Forms.GroupBox gbProgress;
+        private System.Windows.Forms.Label lblProcessingT2P;
+        private System.Windows.Forms.ProgressBar progBarTEXBatch;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.GroupBox gbSettings;
+        private System.Windows.Forms.Button btnSaveLog;
+        private System.Windows.Forms.CheckBox chkShowOnlyNoProcessed;
+        private System.Windows.Forms.Label lblPath;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnSaveOptions;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.TextBox txtTEX2PNGBatchPath;
+        private System.Windows.Forms.Button btnPath;
+        private System.Windows.Forms.SaveFileDialog saveFile;
+    }
+}
---
+++ frmTEXToPNGBatchConversion.cs
@@ -0,0 +1,305 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.IO;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KimeraCS
+{
+
+    using static GDI32;
+    using static FF7TEXTexture;
+    using static FileTools;
+
+    public partial class frmTEXToPNGBatchConversion : Form
+    {
+
+        public bool bCancelPressed;
+
+        public frmTEXToPNGBatchConversion()
+        {
+            InitializeComponent();
+        }
+
+        private void frmTEXToPNGBatchConversion_Load(object sender, EventArgs e)
+        {
+            if (strGlobalPathTEX2PNGBatch == "") strGlobalPathTEX2PNGBatch = strGlobalPath;
+
+            txtTEX2PNGBatchPath.Text = strGlobalPathTEX2PNGBatch;
+
+            bCancelPressed = false;
+        }
+
+        private void btnSaveOptions_Click(object sender, EventArgs e)
+        {
+            strGlobalPathTEX2PNGBatch = txtTEX2PNGBatchPath.Text;
+
+            WriteCFGFile();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnPath_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialogEX fbdTEXBatchPath = new FolderBrowserDialogEX();
+
+            // We must select the directory from where to read the files.
+            fbdTEXBatchPath.folderBrowser.Description =
+                            "Select the source path from where to search the .TEX files:";
+
+            fbdTEXBatchPath.folderBrowser.RootFolder = Environment.SpecialFolder.MyComputer;
+            fbdTEXBatchPath.folderBrowser.ShowNewFolderButton = false;
+
+            if (strGlobalPathTEX2PNGBatch != "")
+            {
+                fbdTEXBatchPath.folderBrowser.SelectedPath = strGlobalPathTEX2PNGBatch;
+            }
+            else
+            {
+                fbdTEXBatchPath.folderBrowser.SelectedPath = strGlobalPath;
+            }
+
+            fbdTEXBatchPath.Tmr.Start();
+            if (fbdTEXBatchPath.folderBrowser.ShowDialog() == DialogResult.OK)
+            {
+                if (fbdTEXBatchPath.folderBrowser.SelectedPath != "")
+                {
+                    // Put Global path.
+                    strGlobalPathTEX2PNGBatch = fbdTEXBatchPath.folderBrowser.SelectedPath;
+                    txtTEX2PNGBatchPath.Text = fbdTEXBatchPath.folderBrowser.SelectedPath;
+                }
+            }
+
+            fbdTEXBatchPath.Dispose();
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            bCancelPressed = false;
+            btnCancel.Enabled = true;
+
+            lblProcessingT2P.Text = "Processing 0000 / 0000";
+            gbProgress.Enabled = true;
+            gbSettings.Enabled = false;
+
+            try
+            {
+                ProcessTEX2PNGBatch(txtTEX2PNGBatchPath.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("There has been some exception error while processing TEX to PNG Batch Conversion feature.",
+                "Error.", MessageBoxButtons.OK);
+
+                //  Reactivate some object to continue working even with an exception
+                bCancelPressed = false;
+
+                ActivateSettings();
+            }
+        }
+
+        public void ActivateSettings()
+        {
+            gbSettings.Enabled = true;
+            gbProgress.Enabled = false;
+        }
+
+        public static Bitmap PutPixelDataIntoBitmap32ARGB(TEX inTEX)
+        {
+            DirectBitmap tmpDBMP = new DirectBitmap(inTEX.width, inTEX.height);
+            int i, iBMPValue, iBMPByteLength, iR, iG, iB, iA;
+
+            iBMPByteLength = inTEX.pixelData.Length / inTEX.bytesPerPixel;
+
+            if (inTEX.hasPal == 1)
+            {
+                for (i = 0; i < iBMPByteLength; i++)
+                {
+                    iR = inTEX.palette[inTEX.pixelData[i] * 4];
+                    iG = inTEX.palette[inTEX.pixelData[i] * 4 + 1];
+                    iB = inTEX.palette[inTEX.pixelData[i] * 4 + 2];
+                    iA = inTEX.palette[inTEX.pixelData[i] * 4 + 3];
+
+                    iBMPValue = (iA << 24) | (iR << 16) | (iG << 8) | iB;
+
+                    tmpDBMP.Bits[i] = iBMPValue;
+                }
+            }
+            else
+            {
+                for (i = 0; i < iBMPByteLength; i++)
+                {
+                    iB = inTEX.pixelData[i * inTEX.bytesPerPixel];
+                    iG = inTEX.pixelData[i * inTEX.bytesPerPixel + 1];
+                    iR = inTEX.pixelData[i * inTEX.bytesPerPixel + 2];
+
+                    if (iR == 0 && iG == 0 && iB == 0) iA = 0;
+                    else iA = 255;
+
+                    iBMPValue = (iA << 24) | (iR << 16) | (iG << 8) | iB;
+
+                    tmpDBMP.Bits[i] = iBMPValue;
+                }
+            }
+
+            return tmpDBMP.Bitmap;
+        }
+
+        public void ProcessTEX2PNGBatch(string strMainPath)
+        {
+            int iCounter;
+            TEX tmpTEX;
+            Bitmap tmpBMP;
+
+            string[] strTEXFilesList;
+            string strSavePNGFileName;
+
+            try
+            {
+                strTEXFilesList = Directory
+                    .EnumerateFiles(strMainPath, "*.*", SearchOption.AllDirectories)
+                    .Where(s => Regex.IsMatch(s.Substring(s.Length - 4, 4).ToUpper(), @".T\d{2}$") ||       // Summon models textures
+                                s.ToUpper().EndsWith("AC") || s.ToUpper().EndsWith("AD") ||                 // Battle models textures
+                                s.ToUpper().EndsWith("AE") || s.ToUpper().EndsWith("AF") ||
+                                s.ToUpper().EndsWith("AG") || s.ToUpper().EndsWith("AH") ||
+                                s.ToUpper().EndsWith("AI") || s.ToUpper().EndsWith("AJ") ||
+                                s.ToUpper().EndsWith("AK") || s.ToUpper().EndsWith("AL") ||
+                                s.ToUpper().EndsWith("TEX")                                                 // Field/World models textures
+                    ).ToArray();
+
+                // Init progressBar
+                iCounter = 1;
+                progBarTEXBatch.Value = iCounter;
+                progBarTEXBatch.Maximum = strTEXFilesList.Count() + 1;
+
+                rtbLog.Clear();
+
+                rtbLog.AppendText("===== Processing TEX2PNG Batch Conversion =====\n");
+                rtbLog.ScrollToCaret();
+
+                foreach (string strTEXFile in strTEXFilesList)
+                {
+                    // Check if cancel has been pressed
+                    if (bCancelPressed) break;
+
+                    tmpTEX = new TEX();
+                    // Load TEX file into variable
+                    if (ReadTEXTexture(ref tmpTEX, strTEXFile) == 0)
+                    {
+                        // Convert to bitmap
+                        tmpBMP = PutPixelDataIntoBitmap32ARGB(tmpTEX);
+
+                        // Save to .png
+                        if (Path.GetExtension(strTEXFile).Length > 2 &&
+                            Path.GetExtension(strTEXFile).ToUpper() != ".TEX")
+                        {
+                            // Case for .T00
+                            strSavePNGFileName = strTEXFile + ".png";
+                        }
+                        else
+                        {
+                            // Other cases
+                            strSavePNGFileName = Path.GetDirectoryName(strTEXFile) + "\\" +
+                                                 Path.GetFileNameWithoutExtension(strTEXFile) +
+                                                 ".png";
+                        }
+                        tmpBMP.Save(strSavePNGFileName, ImageFormat.Png);
+
+                        // Dispose
+                        tmpBMP.Dispose();
+                        UnloadTexture(ref tmpTEX);
+
+                        rtbLog.AppendText("PROCESSED. TEX File: " + strTEXFile +
+                                          ", Entry: " + iCounter.ToString("0000") + ".\n");
+                    }
+                    else
+                    {
+                        rtbLog.AppendText("NOT PROCESSED. TEX File: " + strTEXFile +
+                                          ", Entry: " + iCounter.ToString("0000") + ".\n");
+
+                    }
+
+                    //  Increase progress bar...
+                    iCounter++;
+
+                    progBarTEXBatch.Value = iCounter;
+                    lblProcessingT2P.Text = "Processing... " +
+                                            (iCounter - 1).ToString("0000") + " / " +
+                                            strTEXFilesList.Count().ToString("0000");
+                    lblProcessingT2P.Refresh();
+
+                    rtbLog.ScrollToCaret();
+
+                    //await Task.Delay(150);
+                }
+
+                rtbLog.AppendText("===== Job Finished =====");
+
+                ActivateSettings();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("There has been some exception while doing the TEX2PNG Batch Conversion.",
+                                "Error");
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            bCancelPressed = true;
+            lblProcessingT2P.Text = "Processing 0000 / 0000";
+            progBarTEXBatch.Value = 0;
+
+            ActivateSettings();
+        }
+
+        private void btnSaveLog_Click(object sender, EventArgs e)
+        {
+            // Set filter options and filter index.
+
+            saveFile.Title = "Save TEX2PNG Batch Conversion Log";
+            saveFile.Filter = "Text files|*.txt|All files|*.*";
+            saveFile.FilterIndex = 1;
+            saveFile.FileName = "Kimera_TEX2PNG_batch_conversion.txt";
+
+            // Initial Directory
+            saveFile.InitialDirectory = strGlobalPath;
+
+            try
+            {
+                // Process input if the user clicked OK.
+                if (saveFile.ShowDialog() == DialogResult.OK)
+                {
+                    File.WriteAllText(saveFile.FileName, rtbLog.Text);
+                }
+            }
+            catch
+            {
+                MessageBox.Show("There has been some error saving the TEX2PNG Batch Conversion Log.",
+                                "Error", MessageBoxButtons.OK);
+            }
+        }
+
+        private void frmTEXToPNGBatchConversion_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            bCancelPressed = true;
+
+            progBarTEXBatch.Value = 0;
+            rtbLog.Clear();
+
+            lblProcessingT2P.Text = "Processing... 0000 / 0000";
+            gbSettings.Enabled = true;
+            gbProgress.Enabled = false;
+        }
+    }
+}
