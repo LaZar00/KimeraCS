ed282a4a5c91e7a8497e553966180c896acd7201
David R <david.abril22@gmail.com>
2023-01-21 17:57:54 +0100

    Added NEW FEATURE: Add Joint to field Skeleton Added NEW FEATURE: Edit Joint to field Skeleton

---
+++ frmSkeletonJoints.Designer.cs
@@ -0,0 +1,195 @@
+﻿
+namespace KimeraCS
+{
+    partial class frmSkeletonJoints
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmSkeletonJoints));
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbBoneAttachedTo = new System.Windows.Forms.ComboBox();
+            this.lbLength = new System.Windows.Forms.Label();
+            this.txtLength = new System.Windows.Forms.TextBox();
+            this.lbBoneAttaches = new System.Windows.Forms.Label();
+            this.btnCommit = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.txtBoneName = new System.Windows.Forms.TextBox();
+            this.btnPathRSDFile = new System.Windows.Forms.Button();
+            this.lbRSDFile = new System.Windows.Forms.Label();
+            this.txtRSDFile = new System.Windows.Forms.TextBox();
+            this.openFile = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(49, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Bone Attached to:";
+            // 
+            // cbBoneAttachedTo
+            // 
+            this.cbBoneAttachedTo.FormattingEnabled = true;
+            this.cbBoneAttachedTo.Location = new System.Drawing.Point(148, 21);
+            this.cbBoneAttachedTo.Name = "cbBoneAttachedTo";
+            this.cbBoneAttachedTo.Size = new System.Drawing.Size(164, 21);
+            this.cbBoneAttachedTo.TabIndex = 2;
+            // 
+            // lbLength
+            // 
+            this.lbLength.AutoSize = true;
+            this.lbLength.ForeColor = System.Drawing.SystemColors.Control;
+            this.lbLength.Location = new System.Drawing.Point(99, 72);
+            this.lbLength.Name = "lbLength";
+            this.lbLength.Size = new System.Drawing.Size(43, 13);
+            this.lbLength.TabIndex = 3;
+            this.lbLength.Text = "Length:";
+            // 
+            // txtLength
+            // 
+            this.txtLength.Location = new System.Drawing.Point(148, 69);
+            this.txtLength.Name = "txtLength";
+            this.txtLength.Size = new System.Drawing.Size(164, 20);
+            this.txtLength.TabIndex = 4;
+            // 
+            // lbBoneAttaches
+            // 
+            this.lbBoneAttaches.AutoSize = true;
+            this.lbBoneAttaches.ForeColor = System.Drawing.SystemColors.Control;
+            this.lbBoneAttaches.Location = new System.Drawing.Point(76, 48);
+            this.lbBoneAttaches.Name = "lbBoneAttaches";
+            this.lbBoneAttaches.Size = new System.Drawing.Size(66, 13);
+            this.lbBoneAttaches.TabIndex = 5;
+            this.lbBoneAttaches.Text = "Bone Name:";
+            // 
+            // btnCommit
+            // 
+            this.btnCommit.Location = new System.Drawing.Point(14, 149);
+            this.btnCommit.Name = "btnCommit";
+            this.btnCommit.Size = new System.Drawing.Size(138, 24);
+            this.btnCommit.TabIndex = 7;
+            this.btnCommit.Text = "Commit";
+            this.btnCommit.UseVisualStyleBackColor = true;
+            this.btnCommit.Click += new System.EventHandler(this.btnCommit_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnClose.Location = new System.Drawing.Point(208, 149);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(138, 24);
+            this.btnClose.TabIndex = 8;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // txtBoneName
+            // 
+            this.txtBoneName.Location = new System.Drawing.Point(148, 45);
+            this.txtBoneName.Name = "txtBoneName";
+            this.txtBoneName.Size = new System.Drawing.Size(164, 20);
+            this.txtBoneName.TabIndex = 9;
+            // 
+            // btnPathRSDFile
+            // 
+            this.btnPathRSDFile.Location = new System.Drawing.Point(321, 113);
+            this.btnPathRSDFile.Name = "btnPathRSDFile";
+            this.btnPathRSDFile.Size = new System.Drawing.Size(25, 23);
+            this.btnPathRSDFile.TabIndex = 10;
+            this.btnPathRSDFile.Text = "...";
+            this.btnPathRSDFile.UseVisualStyleBackColor = true;
+            this.btnPathRSDFile.Click += new System.EventHandler(this.btnPathRSDFile_Click);
+            // 
+            // lbRSDFile
+            // 
+            this.lbRSDFile.AutoSize = true;
+            this.lbRSDFile.ForeColor = System.Drawing.SystemColors.Control;
+            this.lbRSDFile.Location = new System.Drawing.Point(12, 99);
+            this.lbRSDFile.Name = "lbRSDFile";
+            this.lbRSDFile.Size = new System.Drawing.Size(49, 13);
+            this.lbRSDFile.TabIndex = 11;
+            this.lbRSDFile.Text = "RSD File";
+            // 
+            // txtRSDFile
+            // 
+            this.txtRSDFile.Location = new System.Drawing.Point(14, 115);
+            this.txtRSDFile.Name = "txtRSDFile";
+            this.txtRSDFile.Size = new System.Drawing.Size(301, 20);
+            this.txtRSDFile.TabIndex = 12;
+            // 
+            // frmSkeletonJoints
+            // 
+            this.AcceptButton = this.btnCommit;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.CancelButton = this.btnClose;
+            this.ClientSize = new System.Drawing.Size(358, 185);
+            this.Controls.Add(this.txtRSDFile);
+            this.Controls.Add(this.lbRSDFile);
+            this.Controls.Add(this.btnPathRSDFile);
+            this.Controls.Add(this.txtBoneName);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnCommit);
+            this.Controls.Add(this.lbBoneAttaches);
+            this.Controls.Add(this.txtLength);
+            this.Controls.Add(this.lbLength);
+            this.Controls.Add(this.cbBoneAttachedTo);
+            this.Controls.Add(this.label1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "frmSkeletonJoints";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Skeleton Joints Data";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmSkeletonJoints_FormClosing);
+            this.Load += new System.EventHandler(this.frmSkeletonJoints_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbBoneAttachedTo;
+        private System.Windows.Forms.Label lbLength;
+        private System.Windows.Forms.TextBox txtLength;
+        private System.Windows.Forms.Label lbBoneAttaches;
+        private System.Windows.Forms.Button btnCommit;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.TextBox txtBoneName;
+        private System.Windows.Forms.Button btnPathRSDFile;
+        private System.Windows.Forms.Label lbRSDFile;
+        private System.Windows.Forms.TextBox txtRSDFile;
+        private System.Windows.Forms.OpenFileDialog openFile;
+    }
+}
---
+++ frmSkeletonJoints.cs
@@ -0,0 +1,449 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+using System.Windows.Forms;
+
+namespace KimeraCS
+{
+    using Defines;
+
+    using static frmSkeletonEditor;
+
+    using static FF7Skeleton;
+    using static FF7FieldSkeleton;
+    using static FF7FieldAnimation;
+    using static FF7FieldRSDResource;
+
+    using static FileTools;
+
+    public partial class frmSkeletonJoints : Form
+    {
+
+        private frmSkeletonEditor frmSkelEdit;
+
+        public static string strAttachedTo, strAttachedName;
+        public static int iMode;                        // Mode: 0 - Add Joint / 1 - Edit Joint
+        public static int iNumBoneSelected;
+
+
+        public frmSkeletonJoints(frmSkeletonEditor frmSkelEdit, int iMenuMode, string strJoint)
+        {
+            InitializeComponent();
+
+            this.frmSkelEdit = frmSkelEdit;
+            Owner = frmSkelEdit;
+
+            iMode = iMenuMode;
+            iNumBoneSelected = -1;
+
+            if (iMode == 1)
+            {
+                strAttachedTo = strJoint.Split('-')[1];
+                strAttachedName = strJoint.Split('-')[0];
+            }
+            else
+            {
+                strAttachedName = "new_bone";
+
+                if (strJoint != "")
+                    strAttachedTo = strJoint.Split('-')[0];
+            }
+        }
+
+        private void FillJointInfo()
+        {
+            int iBoneCounter;
+            float ftmpNum = 0f;
+
+            // Let's fill the combobox with the bones names
+            cbBoneAttachedTo.Items.Clear();
+            txtLength.Text = ftmpNum.ToString("F1", CultureInfo.InvariantCulture.NumberFormat);
+
+            for (iBoneCounter = 0; iBoneCounter < fSkeleton.nBones; iBoneCounter++)
+            {
+                if (fSkeleton.bones[iBoneCounter].joint_i != strAttachedName)
+                {
+                    if (iBoneCounter == 0)
+                    {
+                        cbBoneAttachedTo.Items.Add(fSkeleton.bones[iBoneCounter].joint_f);
+                    }
+                    cbBoneAttachedTo.Items.Add(fSkeleton.bones[iBoneCounter].joint_i);
+                }
+                else iNumBoneSelected = iBoneCounter;
+            }
+
+            cbBoneAttachedTo.SelectedItem = strAttachedTo;
+            txtBoneName.Text = strAttachedName;
+
+            if (iNumBoneSelected != -1)
+                txtLength.Text = fSkeleton.bones[iNumBoneSelected].len.ToString();
+        }
+
+        private void frmSkeletonJoints_Load(object sender, EventArgs e)
+        {
+
+            // We will fill the combo boxes with the list of bones
+            FillJointInfo();
+
+            // We will select the bones to where the joint is connected (if exist any)
+            if (iMode == 1)
+            {
+                txtLength.Enabled = false;
+
+                txtRSDFile.Enabled = false;
+
+                if (fSkeleton.bones[iNumBoneSelected].fRSDResources.Count > 0)
+                    txtRSDFile.Text = fSkeleton.bones[iNumBoneSelected].fRSDResources[0].res_file + ".RSD";
+                else
+                    txtRSDFile.Text = "NO MODEL ASSIGNED";
+
+                btnPathRSDFile.Enabled = false;
+
+                this.Text = "Edit Joint";
+            }
+            else
+            {
+                txtLength.Enabled = true;
+
+                txtRSDFile.Enabled = true;
+                txtRSDFile.Text = "";
+                btnPathRSDFile.Enabled = true;
+
+                this.Text = "Add Joint";
+            }
+        }
+
+        private void ReorderBones()
+        {
+            int iBoneCounter, iMoveCounter, iStoppedCounter, iJumpedCounter;
+
+            List<FieldBone> lstTmpBones = new List<FieldBone>();
+            List<FieldRotation> lstTmpBonesAnim = new List<FieldRotation>();
+
+            iBoneCounter = 0; iMoveCounter = 0; iStoppedCounter = 0; iJumpedCounter = 0;
+
+            while (iBoneCounter < fSkeleton.nBones)
+            {
+                if (iBoneCounter == cbBoneAttachedTo.SelectedIndex)
+                {
+                    // In this case the bones are moved upward
+
+                    iStoppedCounter = iBoneCounter;
+                    iMoveCounter = iNumBoneSelected;
+                    while (iMoveCounter < fSkeleton.nBones - 1 &&
+                           fSkeleton.bones[iMoveCounter].joint_i == fSkeleton.bones[iMoveCounter + 1].joint_f)
+                    {
+                        lstTmpBones.Add(CopyfBone(fSkeleton.bones[iMoveCounter]));
+                        lstTmpBonesAnim.Add(new FieldRotation(fAnimation.frames[0].rotations[iMoveCounter].alpha,
+                                                              fAnimation.frames[0].rotations[iMoveCounter].beta,
+                                                              fAnimation.frames[0].rotations[iMoveCounter].gamma));
+                        iMoveCounter++;
+                        iBoneCounter++;
+                    }
+
+                    // Add last bone if needed
+                    if (iMoveCounter < fSkeleton.nBones)
+                    {
+                        lstTmpBones.Add(CopyfBone(fSkeleton.bones[iMoveCounter]));
+                        lstTmpBonesAnim.Add(new FieldRotation(fAnimation.frames[0].rotations[iMoveCounter].alpha,
+                                                              fAnimation.frames[0].rotations[iMoveCounter].beta,
+                                                              fAnimation.frames[0].rotations[iMoveCounter].gamma));
+                        iMoveCounter++;
+                    }
+
+                    while (iStoppedCounter < iNumBoneSelected)
+                    {
+                        lstTmpBones.Add(CopyfBone(fSkeleton.bones[iStoppedCounter]));
+                        lstTmpBonesAnim.Add(new FieldRotation(fAnimation.frames[0].rotations[iStoppedCounter].alpha,
+                                                              fAnimation.frames[0].rotations[iStoppedCounter].beta,
+                                                              fAnimation.frames[0].rotations[iStoppedCounter].gamma));
+
+                        iStoppedCounter++;
+                        iBoneCounter++;
+                    }
+                }
+                else if (iBoneCounter == iNumBoneSelected)
+                {
+                    // In this case the bones are moved downward
+                    iStoppedCounter = iBoneCounter;
+                    iMoveCounter = iBoneCounter + 1;
+                    iJumpedCounter = 1;
+
+                    // Let's jump the joints we need to move.
+                    while (iMoveCounter < fSkeleton.nBones &&
+                           fSkeleton.bones[iMoveCounter].joint_f == fSkeleton.bones[iMoveCounter - 1].joint_i)
+                    {
+                        iMoveCounter++;
+                        iJumpedCounter++;
+                    }
+
+                    // Add lower joints until selected bone into the new temporary list
+                    while (iMoveCounter < fSkeleton.nBones &&
+                           fSkeleton.bones[iMoveCounter].joint_i != cbBoneAttachedTo.Text)
+                    {
+                        lstTmpBones.Add(CopyfBone(fSkeleton.bones[iMoveCounter]));
+                        lstTmpBonesAnim.Add(new FieldRotation(fAnimation.frames[0].rotations[iMoveCounter].alpha,
+                                                              fAnimation.frames[0].rotations[iMoveCounter].beta,
+                                                              fAnimation.frames[0].rotations[iMoveCounter].gamma));
+
+                        iMoveCounter++;
+                        iBoneCounter++;
+                    }
+
+                    // Let's add last lower joint
+                    lstTmpBones.Add(CopyfBone(fSkeleton.bones[iMoveCounter]));
+                    lstTmpBonesAnim.Add(new FieldRotation(fAnimation.frames[0].rotations[iMoveCounter].alpha,
+                                                          fAnimation.frames[0].rotations[iMoveCounter].beta,
+                                                          fAnimation.frames[0].rotations[iMoveCounter].gamma));
+
+                    // Add jumped bones
+                    while (iJumpedCounter > 0)
+                    {
+                        lstTmpBones.Add(CopyfBone(fSkeleton.bones[iStoppedCounter]));
+                        lstTmpBonesAnim.Add(new FieldRotation(fAnimation.frames[0].rotations[iStoppedCounter].alpha,
+                                                              fAnimation.frames[0].rotations[iStoppedCounter].beta,
+                                                              fAnimation.frames[0].rotations[iStoppedCounter].gamma));
+
+                        iStoppedCounter++;
+                        iBoneCounter++;
+                        iJumpedCounter--;
+                    }
+
+                }
+                else
+                {
+                    // This is the default case where we copy directly the bone
+                    lstTmpBones.Add(CopyfBone(fSkeleton.bones[iBoneCounter]));
+                    lstTmpBonesAnim.Add(new FieldRotation(fAnimation.frames[0].rotations[iBoneCounter].alpha,
+                                                          fAnimation.frames[0].rotations[iBoneCounter].beta,
+                                                          fAnimation.frames[0].rotations[iBoneCounter].gamma));
+                }
+
+                iBoneCounter++;
+            }
+
+            // Assign ordered bones
+            fSkeleton.bones.Clear();
+            fAnimation.frames[0].rotations.Clear();
+            for (iBoneCounter = 0; iBoneCounter < fSkeleton.nBones; iBoneCounter++)
+            {
+                fSkeleton.bones.Add(CopyfBone(lstTmpBones[iBoneCounter]));
+                fAnimation.frames[0]
+                    .rotations.Add(new FieldRotation(lstTmpBonesAnim[iBoneCounter].alpha,
+                                                     lstTmpBonesAnim[iBoneCounter].beta,
+                                                     lstTmpBonesAnim[iBoneCounter].gamma));
+            }
+            lstTmpBones.Clear();
+            lstTmpBonesAnim.Clear();
+        }
+
+
+        //        joint_i joint_f       ==>     joint_i joint_f     ==>     joint_i joint_f
+        //          1 	- 	0                      1 	- 	0                   1 	- 	0
+        //          2 	- 	1                      2 	- 	1                   2 	- 	1
+        //          3 	- 	2                      3 	- 	2                   3 	- 	2
+        //          4 	- 	1                      4 	- 	1                   6   -   2
+        //          5 	- 	4                      5 	- 	4                   7   -   6
+        //          6 	- 	5                      6 	- 	2                   8   -   7
+        //          7 	- 	6                      7 	- 	6                   4   -   1
+        //          8 	- 	7                      8 	- 	7                   5   -   4
+        //          9 	- 	1                      9 	- 	1                   9 	- 	1
+        private void ProcessEditJoint()
+        {
+            int iBoneCounter;
+
+            FieldBone tmpfBone;
+
+            try
+            {
+                // Update the bone data
+                // Assign new updated values
+                tmpfBone = CopyfBone(fSkeleton.bones[iNumBoneSelected]);
+                tmpfBone.joint_i = txtBoneName.Text;
+                tmpfBone.joint_f = cbBoneAttachedTo.Text;
+
+                fSkeleton.bones[iNumBoneSelected] = CopyfBone(tmpfBone);
+
+                // Now we need to update the other instances of "attached to" where
+                // the name is strAttached in case it has been changed.
+                for (iBoneCounter = 0; iBoneCounter < fSkeleton.nBones; iBoneCounter++)
+                {
+                    if (fSkeleton.bones[iBoneCounter].joint_f == strAttachedName)
+                    {
+                        tmpfBone = CopyfBone(fSkeleton.bones[iBoneCounter]);
+                        tmpfBone.joint_f = txtBoneName.Text;
+                        fSkeleton.bones[iBoneCounter] = CopyfBone(tmpfBone);
+                    }
+                }
+
+                // Let's check and/or reorder if needed the joints for good rendering
+                // We must think also that we need to change the rotations.
+                ReorderBones();
+
+                // Update panel model and Combobox of Selected Bone in Skeleton window
+                frmSkelEdit.UpdateBones(txtBoneName.Text + "-" + cbBoneAttachedTo.Text);
+
+                // Refresh local combobox with Bones list
+                strAttachedTo = cbBoneAttachedTo.Text;
+                FillJointInfo();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("There has been some error in the Edit Joint process.", "Error");
+            }
+        }
+
+        // Add Joint is different to Edit Joint because here we will add a new
+        // bone, and if needed or selected, we will leave the choice to the user
+        // to add also the .RSD model.
+        private void ProcessAddJoint()
+        {
+            FieldBone tmpfBone;
+            FieldRSDResource tmpfRSDResource;
+
+            float ftmpLen;
+
+            // First let's check things to be sure we can proceed like:
+            // - bone attached to selected, text bone name, float len or .rsd file)
+            if (cbBoneAttachedTo.SelectedIndex == -1)
+            {
+                MessageBox.Show("You must select some bone where to attach the new joint.", "Information");
+                return;
+            }
+
+            if (txtBoneName.Text == "")
+            {
+                MessageBox.Show("You must write some name for the bone.", "Information");
+                return;
+            }
+
+            if (!float.TryParse(txtLength.Text, NumberStyles.Any, 
+                                CultureInfo.InvariantCulture.NumberFormat, out ftmpLen)) 
+            {
+                MessageBox.Show("Is not possible to parse the specified length.", "Information");
+                return;
+            }
+            
+
+            // We have to create the Bone and/or the .RSD part structure if needed.
+            tmpfBone = new FieldBone();
+            tmpfBone.len = ftmpLen;
+            tmpfBone.joint_f = cbBoneAttachedTo.Text;
+            tmpfBone.joint_i = txtBoneName.Text;
+
+            tmpfBone.nResources = 0;
+            tmpfBone.fRSDResources = new List<FieldRSDResource>();
+
+            tmpfBone.resizeX = 1;
+            tmpfBone.resizeY = 1;
+            tmpfBone.resizeZ = 1;
+
+            // We have to add the .RSD part structure in the bone list of the Skeleton
+            // if the user has selected one
+            if (txtRSDFile.Text != "")
+            {
+                tmpfBone.nResources = 1;
+
+                tmpfRSDResource = new FieldRSDResource(Path.GetFileNameWithoutExtension(txtRSDFile.Text),
+                                                       ref fSkeleton.textures_pool, 
+                                                       Path.GetDirectoryName(txtRSDFile.Text));
+                tmpfBone.fRSDResources.Add(tmpfRSDResource);
+            }
+
+            // Finally we add the Bone/Joint to the Skeleton
+            iNumBoneSelected = fSkeleton.nBones;
+            fSkeleton.bones.Add(tmpfBone);
+            fSkeleton.nBones++;
+
+            // We have to add the new rotation of the new Bone/Joint in the Animation
+            fAnimation.frames[0].rotations.Add(new FieldRotation(0, 0, 0));
+            fAnimation.nBones++;
+
+            // Let's check and/or reorder if needed the joints for good rendering
+            // We must think also that we need to change the rotations.
+            ReorderBones();
+
+            // Update panel model and Combobox of Selected Bone in Skeleton window
+            frmSkelEdit.UpdateBones(txtBoneName.Text + "-" + cbBoneAttachedTo.Text);
+
+            // Refresh local combobox with Bones list
+            strAttachedTo = cbBoneAttachedTo.Text;
+            FillJointInfo();
+        }
+
+        private void btnCommit_Click(object sender, EventArgs e)
+        {
+            if (iMode == 1)
+            {
+                // Edit Joint
+                ProcessEditJoint();
+            }
+            else
+            {
+                // Add Joint
+                ProcessAddJoint();
+            }
+        }
+
+        private void btnPathRSDFile_Click(object sender, EventArgs e)
+        {
+            // Set filter options and filter index.
+            openFile.Title = "Open RSD Resource";
+            openFile.Filter = "FF7 RSD Resource|*.RSD|All files|*.*";
+            openFile.FilterIndex = 1;
+            openFile.FileName = null;
+
+            // Check Initial Directory
+            if (strGlobalPathRSDResourceFolder != null)
+            {
+                openFile.InitialDirectory = strGlobalPathRSDResourceFolder;
+            }
+            else
+            {
+                openFile.InitialDirectory = strGlobalPath;
+            }
+
+            try
+            {
+                // Process input if the user clicked OK.
+                if (openFile.ShowDialog() == DialogResult.OK)
+                {
+                    if (File.Exists(openFile.FileName) && Path.GetExtension(openFile.FileName).ToUpper() == ".RSD")
+                    {
+                        // Set Global Paths
+                        strGlobalPathRSDResourceFolder = Path.GetDirectoryName(openFile.FileName);
+
+                        // Update txtPath variable
+                        txtRSDFile.Text = openFile.FileName;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error opening Model file " + openFile.FileName.ToUpper() + ".",
+                                "Error");
+                return;
+            }
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void frmSkeletonJoints_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            cbBoneAttachedTo.SelectedIndex = -1;
+            strAttachedName = "";
+            strAttachedTo = "";
+        }
+
+    }
+}
