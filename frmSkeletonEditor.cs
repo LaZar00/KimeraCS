26a7e3dae38ea06b5a1fdaa57c4c48fe826aec04
David R <david.abril22@gmail.com>
2023-02-22 17:42:39 +0100

    Versioning

--- frmSkeletonEditor.cs
+++ frmSkeletonEditor.cs
@@ -56,7 +56,7 @@ namespace KimeraCS
     public partial class frmSkeletonEditor : Form
     {
 
-        public const string STR_APPNAME = "KimeraCS 1.6i";
+        public const string STR_APPNAME = "KimeraCS 1.6j";
 
         public static int modelWidth;
         public static int modelHeight;
