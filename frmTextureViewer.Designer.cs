6142053d46861984b23c7ce1ba65246cf62d1ea2
David R <david.abril22@gmail.com>
2023-03-12 19:27:22 +0100

    Update

--- frmTextureViewer.Designer.cs
+++ frmTextureViewer.Designer.cs
@@ -33,6 +33,8 @@ namespace KimeraCS
             this.panelTextureViewer = new System.Windows.Forms.Panel();
             this.pbTextureView = new System.Windows.Forms.PictureBox();
             this.panelButtons = new System.Windows.Forms.Panel();
+            this.lblWidth = new System.Windows.Forms.Label();
+            this.lblHeight = new System.Windows.Forms.Label();
             this.btnZoomIn = new System.Windows.Forms.Button();
             this.btnZoomOut = new System.Windows.Forms.Button();
             this.btnClose = new System.Windows.Forms.Button();
@@ -41,8 +43,6 @@ namespace KimeraCS
             this.btnFlipV = new System.Windows.Forms.Button();
             this.btnFlipH = new System.Windows.Forms.Button();
             this.btnCommit = new System.Windows.Forms.Button();
-            this.lblHeight = new System.Windows.Forms.Label();
-            this.lblWidth = new System.Windows.Forms.Label();
             this.panelTextureViewer.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pbTextureView)).BeginInit();
             this.panelButtons.SuspendLayout();
@@ -57,15 +57,16 @@ namespace KimeraCS
             this.panelTextureViewer.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.panelTextureViewer.Controls.Add(this.pbTextureView);
             this.panelTextureViewer.Location = new System.Drawing.Point(4, 3);
-            this.panelTextureViewer.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panelTextureViewer.Margin = new System.Windows.Forms.Padding(2);
             this.panelTextureViewer.Name = "panelTextureViewer";
             this.panelTextureViewer.Size = new System.Drawing.Size(520, 520);
             this.panelTextureViewer.TabIndex = 2;
             // 
             // pbTextureView
             // 
+            this.pbTextureView.BackColor = System.Drawing.Color.Transparent;
             this.pbTextureView.Location = new System.Drawing.Point(2, 2);
-            this.pbTextureView.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pbTextureView.Margin = new System.Windows.Forms.Padding(2);
             this.pbTextureView.Name = "pbTextureView";
             this.pbTextureView.Size = new System.Drawing.Size(512, 512);
             this.pbTextureView.TabIndex = 1;
@@ -92,12 +93,32 @@ namespace KimeraCS
             this.panelButtons.Size = new System.Drawing.Size(522, 38);
             this.panelButtons.TabIndex = 9;
             // 
+            // lblWidth
+            // 
+            this.lblWidth.AutoSize = true;
+            this.lblWidth.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWidth.Location = new System.Drawing.Point(220, 20);
+            this.lblWidth.Name = "lblWidth";
+            this.lblWidth.Size = new System.Drawing.Size(49, 13);
+            this.lblWidth.TabIndex = 19;
+            this.lblWidth.Text = "W: 1024";
+            // 
+            // lblHeight
+            // 
+            this.lblHeight.AutoSize = true;
+            this.lblHeight.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHeight.Location = new System.Drawing.Point(220, 5);
+            this.lblHeight.Name = "lblHeight";
+            this.lblHeight.Size = new System.Drawing.Size(49, 13);
+            this.lblHeight.TabIndex = 18;
+            this.lblHeight.Text = "H: 1024";
+            // 
             // btnZoomIn
             // 
             this.btnZoomIn.BackgroundImage = global::KimeraCS.Properties.Resources.UV_zoomin;
             this.btnZoomIn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.btnZoomIn.Location = new System.Drawing.Point(180, 2);
-            this.btnZoomIn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnZoomIn.Margin = new System.Windows.Forms.Padding(2);
             this.btnZoomIn.Name = "btnZoomIn";
             this.btnZoomIn.Size = new System.Drawing.Size(34, 34);
             this.btnZoomIn.TabIndex = 17;
@@ -110,7 +131,7 @@ namespace KimeraCS
             this.btnZoomOut.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.btnZoomOut.Enabled = false;
             this.btnZoomOut.Location = new System.Drawing.Point(145, 2);
-            this.btnZoomOut.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnZoomOut.Margin = new System.Windows.Forms.Padding(2);
             this.btnZoomOut.Name = "btnZoomOut";
             this.btnZoomOut.Size = new System.Drawing.Size(34, 34);
             this.btnZoomOut.TabIndex = 16;
@@ -123,7 +144,7 @@ namespace KimeraCS
             this.btnClose.AutoSize = true;
             this.btnClose.DialogResult = System.Windows.Forms.DialogResult.Cancel;
             this.btnClose.Location = new System.Drawing.Point(398, 2);
-            this.btnClose.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnClose.Margin = new System.Windows.Forms.Padding(2);
             this.btnClose.Name = "btnClose";
             this.btnClose.Size = new System.Drawing.Size(120, 34);
             this.btnClose.TabIndex = 15;
@@ -136,7 +157,7 @@ namespace KimeraCS
             this.btnGreen.Appearance = System.Windows.Forms.Appearance.Button;
             this.btnGreen.BackColor = System.Drawing.Color.White;
             this.btnGreen.Location = new System.Drawing.Point(107, 2);
-            this.btnGreen.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnGreen.Margin = new System.Windows.Forms.Padding(2);
             this.btnGreen.Name = "btnGreen";
             this.btnGreen.Size = new System.Drawing.Size(34, 34);
             this.btnGreen.TabIndex = 13;
@@ -148,7 +169,7 @@ namespace KimeraCS
             this.btnRotateUV.BackgroundImage = global::KimeraCS.Properties.Resources.rotateUV_right;
             this.btnRotateUV.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.btnRotateUV.Location = new System.Drawing.Point(72, 2);
-            this.btnRotateUV.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnRotateUV.Margin = new System.Windows.Forms.Padding(2);
             this.btnRotateUV.Name = "btnRotateUV";
             this.btnRotateUV.Size = new System.Drawing.Size(34, 34);
             this.btnRotateUV.TabIndex = 12;
@@ -160,7 +181,7 @@ namespace KimeraCS
             this.btnFlipV.BackgroundImage = global::KimeraCS.Properties.Resources.flipUV_vertically;
             this.btnFlipV.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.btnFlipV.Location = new System.Drawing.Point(37, 2);
-            this.btnFlipV.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnFlipV.Margin = new System.Windows.Forms.Padding(2);
             this.btnFlipV.Name = "btnFlipV";
             this.btnFlipV.Size = new System.Drawing.Size(34, 34);
             this.btnFlipV.TabIndex = 11;
@@ -172,7 +193,7 @@ namespace KimeraCS
             this.btnFlipH.BackgroundImage = global::KimeraCS.Properties.Resources.flipUV_horizontally;
             this.btnFlipH.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.btnFlipH.Location = new System.Drawing.Point(2, 2);
-            this.btnFlipH.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnFlipH.Margin = new System.Windows.Forms.Padding(2);
             this.btnFlipH.Name = "btnFlipH";
             this.btnFlipH.Size = new System.Drawing.Size(34, 34);
             this.btnFlipH.TabIndex = 10;
@@ -184,33 +205,13 @@ namespace KimeraCS
             this.btnCommit.Anchor = System.Windows.Forms.AnchorStyles.Right;
             this.btnCommit.AutoSize = true;
             this.btnCommit.Location = new System.Drawing.Point(274, 2);
-            this.btnCommit.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnCommit.Margin = new System.Windows.Forms.Padding(2);
             this.btnCommit.Name = "btnCommit";
             this.btnCommit.Size = new System.Drawing.Size(120, 34);
             this.btnCommit.TabIndex = 9;
             this.btnCommit.Text = "Commit";
             this.btnCommit.UseVisualStyleBackColor = true;
             this.btnCommit.Click += new System.EventHandler(this.btnCommit_Click);
-            // 
-            // lblHeight
-            // 
-            this.lblHeight.AutoSize = true;
-            this.lblHeight.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblHeight.Location = new System.Drawing.Point(220, 5);
-            this.lblHeight.Name = "lblHeight";
-            this.lblHeight.Size = new System.Drawing.Size(49, 13);
-            this.lblHeight.TabIndex = 18;
-            this.lblHeight.Text = "H: 1024";
-            // 
-            // lblWidth
-            // 
-            this.lblWidth.AutoSize = true;
-            this.lblWidth.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblWidth.Location = new System.Drawing.Point(220, 20);
-            this.lblWidth.Name = "lblWidth";
-            this.lblWidth.Size = new System.Drawing.Size(49, 13);
-            this.lblWidth.TabIndex = 19;
-            this.lblWidth.Text = "W: 1024";
             // 
             // frmTextureViewer
             // 
@@ -225,11 +226,11 @@ namespace KimeraCS
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.KeyPreview = true;
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.MaximizeBox = false;
             this.MinimumSize = new System.Drawing.Size(542, 605);
             this.Name = "frmTextureViewer";
-            this.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Padding = new System.Windows.Forms.Padding(2);
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "TexCoord (UVs) Viewer";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.frmTextureViewer_FormClosed);
